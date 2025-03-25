8eb65d3692267554719b0db2c650d1315c35505f
MiguelAngelBusto <juanadadbatero181@gmail.com>
2025-03-25 13:50:24 -0300

    tp1_1.c ya compilado y funcionando

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,14 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("Hola mundo!!!");
+    int Var1=1;
+    int *puntero=&Var1;
+    printf("\nHola mundo!!!\n\n");
+    printf("\nEl contenido del puntero es %d", *puntero);
+    printf("\nLa dirección de memoria almacenada por el puntero es: %d", puntero);
+    printf("\nLa dirección de memoria de la variable es: %d", &Var1);
+    printf("\nEl tamaño de memoria utilizado por la variable es: %d", sizeof(Var1));
     return 0;
 }
